498c0b97bd6f2ce5be957eb648b6781e022749c3
Francesco Bruno <scobru1988@gmail.com>
2025-07-02 07:17:09 +0200

    fix

--- src/components/ui/ThemeToggle.jsx
+++ src/components/ui/ThemeToggle.jsx
@@ -31,7 +31,7 @@ export const ThemeToggle = () => {
 
   return (
     <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn m-1">
+      <div tabIndex={0} role="button" className="btn-custom m-1">
         {currentTheme === 'dark' ? '🌙' : '☀️'} Theme
         <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
           <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
